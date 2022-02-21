56471b710865cc236f554a3afde9b635ad77fd9e
namdoiz <nnamdi.on7@gmail.com>
2022-02-21 00:33:59 -0600

    after adding an improved join method and keeping score so game ends when either computer or player have won the game five times

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,14 +3,25 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
+def joinor(arr, sep = ', ', for_last_ele = 'or')
+  last_one = arr.pop
+  str = arr.join(sep)
+  puts "#{str.concat(" #{for_last_ele}"," #{last_one}")}"
+end
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts "First to Five"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |   #{brd[3]}  "
@@ -25,29 +36,31 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board # board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+
 end
 
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt("Choose a square (#{empty_squares(brd).join(', ')}):")
-    square =gets.chomp.to_i
+    prompt("Choose a square (#{joinor(empty_squares(brd))}):")
+    square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece! (brd)
+def computer_places_piece!(brd)
   square = empty_squares(brd).sample
   brd[square] = COMPUTER_MARKER
 end
@@ -60,28 +73,33 @@ def someone_won?(brd)
   !!detect_winner(brd)
 end
 
-def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-  winning_lines.each do |line|
-    if brd[line[0]]  == PLAYER_MARKER && 
-       brd[line[1]]  == PLAYER_MARKER &&
-       brd[line[2]]  == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]]  == COMPUTER_MARKER && 
-          brd[line[1]]  == COMPUTER_MARKER &&
-          brd[line[2]]  == COMPUTER_MARKER
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+# if brd[line[0]]  == PLAYER_MARKER &&
+#    brd[line[1]]  == PLAYER_MARKER &&
+#    brd[line[2]]  == PLAYER_MARKER
+#   return 'Player'
+# elsif brd[line[0]]  == COMPUTER_MARKER &&
+#       brd[line[1]]  == COMPUTER_MARKER &&
+#       brd[line[2]]  == COMPUTER_MARKER
+#   return 'Computer'
+# end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
   nil
 end
 
-loop do 
-  board = initialize_board
 
+computer_wins = 0
+player_wins = 0
+
+loop do
+  board = initialize_board
 
   loop do
     display_board(board)
@@ -96,14 +114,29 @@ loop do
   display_board(board)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
+    if detect_winner(board).start_with?('P')
+      player_wins += 1
+      if player_wins == 5
+        prompt "Play again? (y or n)"
+        answer = gets.chomp
+        break unless answer.downcase.start_with?('y')
+      else
+        next
+      end
+    elsif detect_winner(board).start_with?('C')
+      computer_wins += 1
+      if computer_wins == 5
+        prompt "Play again? (y or n)"
+        answer = gets.chomp
+        break unless answer.downcase.start_with?('y')
+      else
+        next
+      end
+    end
   end
 
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
-end
 
+end
+prompt "Player won #{player_wins} times."
+prompt "Computer won #{computer_wins} times."
 prompt 'Thanks for playing TIC TAC TOE! Goodbye.'
